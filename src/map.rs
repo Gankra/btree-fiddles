203e81d897ffdaa2348c9e2502eaf9c6be2c6108
Alexis Beingessner <a.beingessner@gmail.com>
2015-02-11 13:36:57 -0500

    update to master

--- src/map.rs
+++ src/map.rs
@@ -461,7 +461,7 @@ impl<K: Ord, V> BTreeMap<K, V> {
 }
 
 impl<K, V> IntoIterator for BTreeMap<K, V> {
-    type Iter = IntoIter<K, V>;
+    type IntoIter = IntoIter<K, V>;
 
     fn into_iter(self) -> IntoIter<K, V> {
         self.into_iter()
@@ -469,7 +469,7 @@ impl<K, V> IntoIterator for BTreeMap<K, V> {
 }
 
 impl<'a, K, V> IntoIterator for &'a BTreeMap<K, V> {
-    type Iter = Iter<'a, K, V>;
+    type IntoIter = Iter<'a, K, V>;
 
     fn into_iter(self) -> Iter<'a, K, V> {
         self.iter()
@@ -477,7 +477,7 @@ impl<'a, K, V> IntoIterator for &'a BTreeMap<K, V> {
 }
 
 impl<'a, K, V> IntoIterator for &'a mut BTreeMap<K, V> {
-    type Iter = IterMut<'a, K, V>;
+    type IntoIter = IterMut<'a, K, V>;
 
     fn into_iter(mut self) -> IterMut<'a, K, V> {
         self.iter_mut()
